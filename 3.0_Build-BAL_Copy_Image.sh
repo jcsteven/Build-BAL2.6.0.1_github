a2dabac2b56154b9c437bc35831df386b934eed4
jcsteven <jcsteven.yu@gmail.com>
2018-09-10 08:48:16 +0800

    Fix the script fail to copy the ONL image to the specified folder

--- 3.0_Build-BAL_Copy_Image.sh
+++ 3.0_Build-BAL_Copy_Image.sh
@@ -20,29 +20,29 @@ IMAGE_NAME=amd64-${TODAY}-BAL-Build
 TO_COPY_ONL_IMAGE="y"
 if [[ "${TO_COPY_ONL_IMAGE}" == "y" ]]; then
 	echo "To Copy ONL Image File."
-	BUILD_DIR=./${ONL_NAME}/RELEASE/jessie/amd64
-	if [ ! -d ${BAL_BUILD_DIR} ]; then
-	   echo $LINENO "missing ${BAL_BUILD_DIR}" |  tee -a $Record_File
+	BUILD_DIR1=./${ONL_NAME}/RELEASE/jessie/amd64
+	if [ ! -d ${BUILD_DIR1} ]; then
+	   echo $LINENO "missing ${BUILD_DIR1}" |  tee -a $Record_File
 	   exit -1
 	fi		
 	#-- Create build imgage folder
-    #cp ${BUILD_DIR}/ONL-2.0.0_ONL-OS_*_AMD64_INSTALLED_INSTALLER ./${IMAGE_NAME}
-    #cp ${BUILD_DIR}/ONL-master_ONL-OS_*_AMD64_INSTALLED_INSTALLER ./${IMAGE_NAME}
-    cp ${BUILD_DIR}/ONL-asxvolt16_ONL-OS_*_AMD64_INSTALLED_INSTALLER ./${IMAGE_NAME}
+    #cp ${BUILD_DIR1}/ONL-2.0.0_ONL-OS_*_AMD64_INSTALLED_INSTALLER ./${IMAGE_NAME}
+    #cp ${BUILD_DIR1}/ONL-master_ONL-OS_*_AMD64_INSTALLED_INSTALLER ./${IMAGE_NAME}
+    cp ${BUILD_DIR1}/ONL-asxvolt16_ONL-OS_*_AMD64_INSTALLED_INSTALLER ./${IMAGE_NAME}
 fi 
 
 TO_COPY_BAL_IMAGE="y"
 if [[ "${TO_COPY_BAL_IMAGE}" == "y" ]]; then 
 	echo "To Copy BAL Image File."	
-	#BUILD_DIR=./${ONL_NAME}/${BAL_NAME}/bal_release/3rdparty/maple/cur/asfvolt16/release
-	BUILD_DIR=./${ONL_NAME}/${BAL_NAME}/bal_release/3rdparty/bcm-sdk/rc/asfvolt16/release
+	#BUILD_DIR2=./${ONL_NAME}/${BAL_NAME}/bal_release/3rdparty/maple/cur/asfvolt16/release
+	BUILD_DIR2=./${ONL_NAME}/${BAL_NAME}/bal_release/3rdparty/bcm-sdk/rc/asfvolt16/release
 	if [ ! -d ${BAL_BUILD_DIR} ]; then
-	   echo $LINENO "missing ${BAL_BUILD_DIR}" |  tee -a $Record_File
+	   echo $LINENO "missing ${BUILD_DIR2}" |  tee -a $Record_File
 	   exit -1
 	fi			
 	
 	#-- Create build imgage folder
-    cp ${BUILD_DIR}/*.tar.gz ./${IMAGE_NAME}
+    cp ${BUILD_DIR2}/*.tar.gz ./${IMAGE_NAME}
 fi
 
 
